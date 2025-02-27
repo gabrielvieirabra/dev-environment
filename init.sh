fdcf06e1a5d37d3d2efa985241210a73fd8c0e49
Gabriel Sousa <gabrielvieirabra@gmail.com>
2025-02-27 14:38:33 -0300

    feat: brew stuff

--- init.sh
+++ init.sh
@@ -1,23 +1,51 @@
 #!/usr/bin/env bash
 
-
 # foundation: brew, webi, git, zsh, oh-my-zsh, ghostty, tmux, neovim, aerospace
 # ci/cd: gh, act
 # iac: terraform, terragrunt, tfenv
-# containers: colima, k9s, kind/minikube
+# containers: colima, k9s, kind/minikube, argocd
 # programming: python3, golang
 # cloud: aws-cli, awsless, eksctl
 # misc: ripgrep, fd, pathman, aliasman, lsd, curlie, jq, yq, shellcheck, shfmt
 
+setup_zsh() {
+    brew install zsh && chsh -s $(which zsh); zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
 
-misc="brew, webi, ripgrep, fd, pathman, aliasman, lsd, curlie, jq, yq, shellcheck, shfmt"
-set -e
-for i in $misc; do
-    echo "Installing $i"
-    curl -sS https://webi.sh/$i | sh
-    if [ $? -ne 0 ]; then
-        trap 'echo "exit $? due to error"' EXIT
-    fi
-    echo "Sourcing $i"
-    source ~/.config/envman/PATH.env
-done
+install_with_brew () {
+    # make these better, how ?
+    brew tap hashicorp/tap && brew install hashicorp/tap/terraform
+    brew tap weaveworks/tap && brew install weaveworks/tap/eksctl
+    brew install --cask nikitabobko/tap/aerospace
+
+    local foudation="ghostty, tmux, neovim, gh, act, opentofu, terragrunt,
+    tfenv, colima, k9s, kind, argocd"
+    for k in $foudation; do
+    if brew info --cask "$k" &>/dev/null; then
+        brew install --cask $k
+    elif brew info "$k" &>/dev/null; then
+        brew install $k
+    else
+        echo "package doesn't exists"
+    done
+}
+
+install_with_webi(){
+    local misc="brew, git, webi, ripgrep, fd, pathman, aliasman, lsd,
+    curlie, jq, yq, shellcheck, shfmt, awsless"
+    set -e
+    for i in $misc; do
+        echo "Installing $i"
+        curl -sS https://webi.sh/$i | sh
+        if [ $? -ne 0 ]; then
+            trap 'echo "exit $? due to error"' EXIT
+        fi
+        echo "Sourcing $i"
+        source ~/.config/envman/PATH.env
+    done
+}
+
+install_with_webi()
+install_with_brew()
+setup_zsh()
